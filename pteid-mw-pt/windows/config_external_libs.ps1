c496bcfa7194f8dc5d718c0d7047731024d8d7e9
Diogo Nascimento <dnascimento@kenetto.com>
2022-04-27 02:25:19 +0100

    Triiger external-libs

--- pteid-mw-pt/windows/config_external_libs.ps1
+++ pteid-mw-pt/windows/config_external_libs.ps1
@@ -4,3 +4,6 @@ $externalLibsFolder = "${currentLocation}/pteid-mw-pt/windows/external-libs/"
 Get-ChildItem "${externalLibsFolder}" -Recurse -Directory | ForEach-Object {
   & "${externalLibsFolder}$_/setup.ps1" | Out-Default
 }
+
+
+# Change the cache
