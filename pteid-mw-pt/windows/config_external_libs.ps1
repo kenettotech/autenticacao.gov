8d35da23bd9a1a09204805d4e5252ed1dae54eb8
Diogo Nascimento <dnascimento@kenetto.com>
2022-04-27 01:49:39 +0100

    Add full path

--- pteid-mw-pt/windows/config_external_libs.ps1
+++ pteid-mw-pt/windows/config_external_libs.ps1
@@ -1,5 +1,5 @@
 $currentLocation=Get-Location
-$externalLibsFolder = "${currentLocation}/external-libs" | Out-Null
+$externalLibsFolder = "${currentLocation}/pteid-mw-pt/windows/external-libs" | Out-Null
 
 Get-ChildItem "${externalLibsFolder}" -Recurse -Directory | ForEach-Object {
   & $_/setup.ps1 | Out-Null
--- pteid-mw-pt/windows/install_platform_sdk.ps1
+++ pteid-mw-pt/windows/install_platform_sdk.ps1
@@ -1,6 +1,6 @@
 $currentLocation=Get-Location
-New-Item -Path "${currentLocation}" -Name "platform_sdk" -ItemType "directory" | Out-Null
+New-Item -Path "${currentLocation}/pteid-mw-pt/windows/" -Name "platform_sdk" -ItemType "directory" | Out-Null
 
-Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2173743" -OutFile "platform_sdk\setup.exe" | Out-Null
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2173743" -OutFile "pteid-mw-pt/windows/platform_sdk\setup.exe" | Out-Null
 
-Start-Process -Wait -FilePath .\platform_sdk\cdpksetup.exe -Argument "/silent"
+Start-Process -Wait -FilePath .\pteid-mw-pt/windows\platform_sdk\cdpksetup.exe -Argument "/silent"
