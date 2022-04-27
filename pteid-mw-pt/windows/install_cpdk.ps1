885a8605012572b5c910ae623c8f59b3e566b507
Diogo Nascimento <dnascimento@kenetto.com>
2022-04-27 01:59:02 +0100

    Check if folder exists before creating

--- pteid-mw-pt/windows/install_cpdk.ps1
+++ pteid-mw-pt/windows/install_cpdk.ps1
@@ -1,5 +1,7 @@
 $currentLocation=Get-Location
-New-Item -Path "${currentLocation}/pteid-mw-pt/windows/" -Name "cpdk" -ItemType "directory"  | Out-Null
+if (-Not Test-Path "${currentLocation}/pteid-mw-pt/windows/cpdk") {
+	New-Item -Path "${currentLocation}/pteid-mw-pt/windows/" -Name "cpdk" -ItemType "directory"  | Out-Null
+}
 
 Invoke-WebRequest -Uri "https://download.microsoft.com/download/1/7/6/176909B0-50F2-4DF3-B29B-830A17EA7E38/CPDK_RELEASE_UPDATE/cpdksetup.exe" -OutFile "pteid-mw-pt/windows/cpdk\cpdksetup.exe" | Out-Null
 
--- pteid-mw-pt/windows/install_platform_sdk.ps1
+++ pteid-mw-pt/windows/install_platform_sdk.ps1
@@ -1,5 +1,7 @@
 $currentLocation=Get-Location
-New-Item -Path "${currentLocation}/pteid-mw-pt/windows/" -Name "platform_sdk" -ItemType "directory" | Out-Null
+if (-Not Test-Path "${currentLocation}/pteid-mw-pt/windows/platform_sdk") {
+	New-Item -Path "${currentLocation}/pteid-mw-pt/windows/" -Name "platform_sdk" -ItemType "directory" | Out-Null
+}
 
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2173743" -OutFile "pteid-mw-pt/windows/platform_sdk\setup.exe" | Out-Null
 
